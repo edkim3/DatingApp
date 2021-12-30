725b0ca4bdb13657d47052b267aed0cd3201a7e8
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-12-29 20:50:02 -0700

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,5 +11,8 @@ namespace API.Entities
 
         public string Username { get; set; }
         
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
