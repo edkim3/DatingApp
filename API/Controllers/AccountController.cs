5d5f3e0d26c6540377b74b1385528f4c1f967242
edkim3 <94409377+edkim3@users.noreply.github.com>
2022-01-09 17:10:58 -0700

    End of Section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,8 +51,8 @@ namespace API.Controllers
             {
                 Username = user.Username,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
-
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -79,8 +79,8 @@ namespace API.Controllers
                 Username = user.Username,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
-
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
