616c6b95583e7be2ab7318fa0db12e9346e6702a
edkim3 <94409377+edkim3@users.noreply.github.com>
2022-01-10 17:40:32 -0700

    End of Section 14

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -34,7 +34,7 @@ namespace API.Controllers
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
             var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
-            userParams.CurrentUsername = User.GetUsername();
+            userParams.CurrentUsername = user.Username;
             if (string.IsNullOrEmpty(userParams.Gender))
                 userParams.Gender = user.Gender == "male" ? "female" : "male";
 
